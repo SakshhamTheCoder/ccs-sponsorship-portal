220673a2ecda3cd80f1f1f6ab9a0e1302f57f2c9
SakshhamTheCoder <sakshhamtg@gmail.com>
2025-01-25 00:35:28 +0530

    Admin dashboard

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("login", admin_login, name="admin_login"),
-    path("main", dashboard, name="main"),
-    path("logout", admin_logout, name="admin_logout"),
+    path("", dashboard, name="dashboard"),
+    path("logout/", logout_view, name="logout"),
+    path("login/", login_view, name="login"),
 ]
--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -1,34 +1,42 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
 from django.contrib import messages
 
+from payments.models import Payment
 
-def admin_login(request):
+
+@login_required
+@user_passes_test(
+    lambda u: u.is_staff, login_url="login"
+)  # Redirect to login if not staff
+def dashboard(request):
+    payments = Payment.objects.filter(sponsor__payment_success=True).select_related(
+        "sponsor"
+    )
+    return render(request, "dashboard/dashboard.html", {"payments": payments})
+
+
+def login_view(request):
     if request.method == "POST":
-        email = request.POST.get("email")
+        username = request.POST.get("username")
         password = request.POST.get("password")
-        user = authenticate(request, email=email, password=password)
-
-        if user and user.is_staff:
-            login(request, user)
-            return redirect("main")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            if user.is_staff:
+                auth_login(request, user)
+                return redirect("dashboard")
+            else:
+                messages.error(
+                    request, "You are not authorized to access the admin dashboard."
+                )
         else:
-            messages.error(request, "Invalid credentials or not authorized.")
-
+            messages.error(request, "Invalid username or password.")
     return render(request, "dashboard/login.html")
 
 
 @login_required
-@user_passes_test(lambda u: u.is_staff)
-def dashboard(request):
-    return render(request, "dashboard/dashboard.html")
-
-
-def admin_logout(request):
-    logout(request)
-    return redirect("admin_login")
+def logout_view(request):
+    auth_logout(request)
+    return redirect("login")  # Redirect to the custom login page
